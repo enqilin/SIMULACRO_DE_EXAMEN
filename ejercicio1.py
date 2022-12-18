2d9e110459509f3dd3e9a9c4c32b44d78ddb6c70
enqilin <114181724+enqilin@users.noreply.github.com>
2022-12-18 22:08:49 +0000

    ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+import random
+boleto=(random.randint())
+def luck_check():
+    luck=[]
+    luck.append(boleto)
+    for i in luck:
+        if len(luck)%2!=0:
+
+
+if __name__=='__main__':
+    main()
