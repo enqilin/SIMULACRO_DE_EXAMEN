583a2b978c88152cfa24181ed50e4894c4dbdc51
enqilin <114181724+enqilin@users.noreply.github.com>
2022-12-18 23:01:22 +0000

    ejercicio2

---
+++ ejercicio2.py
@@ -0,0 +1,10 @@
+from ast import main 
+
+def numero(int):
+    for indice ,letra in enumerate(int):
+        del (int[letra])
+       
+    return int
+numero(input("ingrese un numero: "))
+if __name__ == "__main__":
+    main()
